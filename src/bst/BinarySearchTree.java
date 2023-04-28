9ff558f375943ce9f64631b3463402404e0a42c9
Nathan <nathan.choi81@gmail.com>
2023-04-28 14:47:29 -0700

    updated traversals code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -116,19 +116,26 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Traverse the tree in an preorder fashion
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
+		preOrderRecurse(root);
 		System.out.println("PreOrder test commit");
-		preOrderRecurse(root); 
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
-		
+		System.out.println("PreOrder test commit");
+		if (node == null) {
+			return;
+		}
+		else{
+			System.out.print(node.data + " ");
+			preOrderRecurse(node.leftChild);
+			preOrderRecurse(node.rightChild);
+		}
 	}
 	
 	//Traverse the tree in an preorder fashion but using a stack
 	//Print the current node first and then recurse on the children
 	public void preOrderStack() {
 		Stack<BSTNode<T>> pre = new Stack<BSTNode<T>>();
-		
 	}
 		
 
@@ -137,12 +144,19 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//then recursively print the right side of current node
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
-		System.out.println("InOrder test commit");
 		inOrderRecurse(root); 
+		System.out.println("InOrder test commit");
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null) {
+		return;
+		}
+		else{
+			inOrderRecurse(node.leftChild);
+			System.out.print(node.data + " ");
+			inOrderRecurse(node.rightChild);
+		}
 	}
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
@@ -154,12 +168,19 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Traverse the tree in an postorder fashion
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
+		postOrderRecurse(root);
 		System.out.println("PostOrder test commit");
-		postOrderRecurse(root); 
 	}
 	
 	public void postOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null) {
+			return;
+		}
+		else{
+			postOrderRecurse(node.leftChild);
+			postOrderRecurse(node.rightChild);
+			System.out.print(node.data + " ");
+		}
 	}
 	
 	//Traverse the tree in an postorder fashion uses Stacks. 
